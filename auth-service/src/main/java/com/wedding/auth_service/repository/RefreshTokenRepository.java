002c89ce2f610b676d49b688ea7f030c0f6a8ca4
ShanPrabod <165180388+ShanPrabod@users.noreply.github.com>
2025-12-15 19:54:32 +0530

    Auth service tested
    Tested user registration, Login with Registered User, Access Protected Endpoint WITHOUT Token, Access Protected Endpoint WITH Valid Token, Test Token Expiration

--- auth-service/src/main/java/com/wedding/auth_service/repository/RefreshTokenRepository.java
+++ auth-service/src/main/java/com/wedding/auth_service/repository/RefreshTokenRepository.java
@@ -3,9 +3,14 @@ package com.wedding.auth_service.repository;
 import com.wedding.auth_service.entity.RefreshToken;
 import com.wedding.auth_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+    
+    @Modifying
+    @Transactional
     void deleteByUser(User user);
 }
